9bd1200fa0c76bb9660eae53076035379abdc35e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-19 14:07:32 -0300

    Estilos CSS

--- 01-intro-react/src/main.jsx
+++ 01-intro-react/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
 import { NuevoComponente } from './NuevoComponente';
+//Importamos los estilos CSS como compenente global
+import './styles.css';
+
 
 /** Renderisamos nuestra aplicacion */
 ReactDOM.createRoot( document.getElementById('root') ).render(
