895d239da56326ca07fe77bdfe34c7306c37b137
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-18 21:30:15 -0300

    Primer componente React

---
+++ 01-intro-react/src/FirstApp.jsx
@@ -0,0 +1,13 @@
+// export function App() {
+//     return (
+//         <h1>Hola mundo!!!</h1>
+//     );
+// }
+
+// import React from 'react'
+
+export const FirstApp = () => {
+  return (
+    <h1>FirstApp</h1>
+  )
+}
--- 01-intro-react/src/main.jsx
+++ 01-intro-react/src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function App() {
-    return (
-        <h1>Hola mundo</h1>
-    );
-}
+import { FirstApp } from './FirstApp';
 
 /** Renderisamos nuestra aplicacion */
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <App />
+        <FirstApp />
     </React.StrictMode>
 );
