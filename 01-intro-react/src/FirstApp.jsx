2c8d8a11aa4446c9fe758dc9793c740562d9c04a
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-19 13:21:00 -0300

    Impresion de variables Javascript

--- 01-intro-react/src/FirstApp.jsx
+++ 01-intro-react/src/FirstApp.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 // export function App() {
 //     return (
 //         <h1>Hola mundo!!!</h1>
@@ -5,19 +6,41 @@
 // }
 
 // import React from 'react'
+/**
+ * Los objetos no pueden pasarse como variables dentro de un Functional component
+ * lo que se puede hacer es pasarlos con JSON.stringify( Objeto ), de esta manera
+ * el objeto se transforma en JSON con lo cual nuestro FC lo puede leer sin problema
+ */
 
-import React from 'react'
+// Objeto
+const objeto = {
+  mensaje:'Bienvenido ',
+  empleado:'Santiago Quiroz'
+};
 
+/**
+ * Las funciones tambien se pueden pasar como variable dentro de nuestro FC, aunque
+ * estas pueden ir dentro del FC se recomienda siempre ponerlas por fuera
+ */
+
+//Funcion
+const funcion = (a,b)=> {
+  return a + b; 
+}
+
+// Componente funcional
 export const FirstApp = () => {
   return (
     <>
-      <h1>
-        Santiago Quiroz
-      </h1>
-      <h2>
-        Ingeniero de software
-      </h2>
-      
+      <h1> Santiago Quiroz </h1>
+      <h2> Ingeniero de software </h2>
+      {/**
+       * De esta manera podemos pasar un objeto como varible 
+       * dentro de nuestro functional component
+       */}
+      {/* <code>{ JSON.stringify( objeto )}</code> */}
+
+      {/* <h2>{funcion(3,6)}</h2> */}
     </>
   )
 }
