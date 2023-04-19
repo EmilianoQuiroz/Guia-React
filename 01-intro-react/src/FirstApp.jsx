91a7db9d8c0fd893941c9880af3d2a0d535b6f88
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-19 13:01:52 -0300

    Functional components

--- 01-intro-react/src/FirstApp.jsx
+++ 01-intro-react/src/FirstApp.jsx
@@ -6,8 +6,18 @@
 
 // import React from 'react'
 
+import React from 'react'
+
 export const FirstApp = () => {
   return (
-    <h1>FirstApp</h1>
+    <>
+      <h1>
+        Santiago Quiroz
+      </h1>
+      <h2>
+        Ingeniero de software
+      </h2>
+      
+    </>
   )
 }
---
+++ 01-intro-react/src/NuevoComponente.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NuevoComponente = () => {
+  return (
+    <h2>Santiago Quiroz</h2>
+  )
+}
