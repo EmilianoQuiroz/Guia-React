b0aba7f5dae7491727458bb79bce2e8e9c0d4ca2
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-21 19:50:44 -0300

    Ejercicio contador

--- 01-intro-react/src/main.jsx
+++ 01-intro-react/src/main.jsx
@@ -4,11 +4,13 @@ import { FirstApp } from './FirstApp';
 import { NuevoComponente } from './NuevoComponente';
 //Importamos los estilos CSS como compenente global
 import './styles.css';
+import { CounterApp } from './CounterApp';
 
 
 /** Renderisamos nuestra aplicacion */
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp />
+        {/* <FirstApp /> */}
+        <CounterApp value = { 10 }/>
     </React.StrictMode>
 );
