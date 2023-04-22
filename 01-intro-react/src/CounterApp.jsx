2cb0ea53bda49c156e97c759e33ea0b61b51cb2b
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-22 12:42:18 -0300

    Hooks: useState

--- 01-intro-react/src/CounterApp.jsx
+++ 01-intro-react/src/CounterApp.jsx
@@ -1,16 +1,24 @@
+//Importamos los Hooks
+import { useState } from 'react'
 import React from 'react'
 import PropTypes from 'prop-types';// Importamos los proptypes
 
 export const CounterApp = ({ value }) => {
  
+    const [ counter, setCounter] = useState( value );
     const sumarUnidad = () => { 
-        console.log('+1')
+        // console.log('+1')
+        /**
+         * Cuando mandamos a llamar al setCounter le estamos diciendo a React que el
+         * estado cambio, en este caso se le suma uno en cada render
+         */
+        setCounter( counter + 1);
     }
 
     return (
     <>
         <h1>CounterApp</h1>
-        <h2> { value } </h2>
+        <h2> { counter } </h2>
         <button onClick={ sumarUnidad }>
             +1
         </button>
